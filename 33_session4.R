b15cb07e838efa3be387b4c4579da83c3d13af1a
Suman Paudel <dastonsuman1997@gmail.com>
2024-03-03 17:27:58 +0545

    added some files

--- 33_session4.R
+++ 33_session4.R
@@ -1,12 +1,11 @@
 # column vector
 x <- c(1:30)
-y <- x^3
-plot(x,y)
-
-
+y <- x ^ 3
+plot(x, y)
 
 # store the current working using following command:
 initial.dir <- getwd()
+
 # to change the working directory your custom directory
 setwd("C:/Users/SumanPaudel/Desktop/R For Data Science")
 
@@ -20,8 +19,9 @@ sink('session3.out')
 iris <- read.csv('iris.csv')
 
 # plot the iris dataset to do some analysis
-plot(iris)
 #  this won't appear in R studio as output is being saved in session3.out file
+plot(iris)
+
 
 # summary of iris dataset
 summary(iris)
@@ -38,18 +38,27 @@ setwd(initial.dir)
 
 # extracting data from internet to use pipe
 # load the iris data from UCI machine learning repo
-iris <- read.csv(url('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'),header=FALSE)
+iris <-
+  read.csv(
+    url(
+      'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    ),
+    header = FALSE
+  )
 
 # look first few values of data
 head(iris)
 
 # add column names for V1, V2, V3, V4, V5 columns to iris dataframe
-names(iris) <- c("Sepal.Length", "Sepal.Width", "Petal.Length", 
-                 "Petal.Width", "Species")
+names(iris) <- c("Sepal.Length",
+                 "Sepal.Width",
+                 "Petal.Length",
+                 "Petal.Width",
+                 "Species")
 
 # saving the downloaded file to csv in local system
 # write.csv(dataframe_name, "path\file_name.csv", row.names=FALSE)
-write.csv(iris,'iris.csv')
+write.csv(iris, 'iris.csv')
 
 # check your working directory using:
 getwd()
@@ -63,33 +72,34 @@ iris$Sepal.Length.SQRT <- iris$Sepal.Length %>% sqrt()
 
 # Compute the square root of `iris$Sepal.Length` and assign it to the same variable
 # The assignment pipe, %<>%, is used to update a value by first piping it into one or more rhs expressions
-iris$Sepal.Length %<>% sqrt
 # while using %<>%, we must be careful as original data will be lost.
 # It's a good practice to store the computed value in new column
+iris$Sepal.Length %<>% sqrt
+
 
 # The “tee” pipe operator “%T%”
 set.seed(123)
 # setting random seed to generate random number that can be reproduced
 
 # rnorm generated the random value from normal distribution
-rnorm(200) %>% 
-  matrix(ncol = 2) %>% 
-  plot %>% 
+rnorm(200) %>%
+  matrix(ncol = 2) %>%
+  plot %>%
   colSums
 # the above code didn't run because in R after plot no code will be executed because it won't return anything
 
 # pipe `tee` in action
-rnorm(200) %>% 
-  matrix(ncol = 2) %T>% 
-  plot %>% 
+rnorm(200) %>%
+  matrix(ncol = 2) %T>%
+  plot %>%
   colSums
 
 # Here %T>% is useful when an expression is used for its side-effect, in our case plotting.
 # we are able to get the column sum even after plotting.
 
 # The exposing pipe operator “%$%”
-iris %>% 
-  subset(Sepal.Length > mean(Sepal.Length)) %$% 
+iris %>%
+  subset(Sepal.Length > mean(Sepal.Length)) %$%
   cor(Sepal.Length, Sepal.Width)
 # it gives the correlation of subset of values on which sepal length is greater than mean of sepal length
 # This is useful when functions do not have a built-in data argument.
@@ -105,4 +115,4 @@ cor(iris$Sepal.Length, iris$Sepal.Width)
 # Final Notes:
 # %>% are use widely in R
 # %<>% used when assignment of variables are required
-# %T% and %$% use when required explicitly on rare occasion. 
+# %T% and %$% use when required explicitly on rare occasion.
