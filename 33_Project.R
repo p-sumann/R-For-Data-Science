209390a7ce35ffed14c77e45c0881e1d499a2f81
Suman Paudel <dastonsuman1997@gmail.com>
2024-03-19 19:35:44 +0545

    web scraping

--- 33_Project.R
+++ 33_Project.R
@@ -5,3 +5,13 @@
 
 
 # Task 3
+
+# Data Mining
+# import library pdftools
+
+base_url <- "https://scholar.google.com/scholar?start=10&hl=en&q="
+search_term <- "Data+Mining"
+url <- paste0(base_url, search_term)
+url
+
+
