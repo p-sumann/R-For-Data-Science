6c21b18cfb12b076ae4d9d884f43bd069e03bc05
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-30 22:59:45 +0545

    Some Changes

--- FDS Assignment 2.R
+++ FDS Assignment 2.R
@@ -66,7 +66,7 @@ make_price <- as_tibble(aggregate(price ~ make, auto_data, mean))
 make_price$price <- as.integer(make_price$price)
 
 ggplot(make_price, aes(x = make, y = price)) +
-  geom_bar(stat = "identity", fill = "#133444") +
+  geom_bar(stat = "identity", fill = "steelblue") +
   geom_text(aes(label = price), vjust = -0.5) + 
   theme_minimal() +
   theme(
@@ -86,14 +86,6 @@ ggplot(hp_cyl, aes(x = horsepower, y = num_cylinders)) +
     plot.title = element_text(hjust = 0.5))+
   labs(x = "Horse Power", y = "Number of Cylinders", title = "Horsepower by Number of Cylinders")
 
-# make vs price
-make_price <- as.data.frame(aggregate(make ~ price, data = auto_data, FUN = mean))
-make_price$price <- as.integer(make_price$price)
-ggplot(data = make_price, aes(x = make, y = price)) + 
-  geom_bar(stat ='identity',fill = "orange") + 
-  theme(axis.text.x = element_text(angle = 40,vjust = 0.5,hjust = 1)) + 
-  geom_text(aes(label = price, vjust = 1.5)) + ggtitle("Make by Price") + ylim(0,40000)
-
 # cylinder vs mpg (city, highway)
 mpg_summary <- auto_data %>%
   group_by(num_cylinders) %>%
@@ -110,6 +102,7 @@ mpg_summary %>%
   labs(x = "Number of Cylinders", y = "Mean MPG", fill = "MPG Type") + 
   theme_classic()
 
+# relationship between horsepower and price
 ggplot(auto_data, aes(x = horsepower, y = price, color = fuel_type)) + 
   geom_point(alpha = 0.5) + 
   labs(x = "Horsepower", y = "Price", color = "Fuel Type") + 
@@ -124,8 +117,8 @@ ggplot(auto_data, aes(x = horsepower, y = price, color = fuel_type)) +
   ggtitle("Relationship between Horsepower and Price")
 
 # relationship between engine size and price with respect to fuel type
-ggplot(data=auto_data, aes(x= price, y= engine_size))+
-  +     geom_point(aes(color=fuel_type, shape=fuel_type))
+ggplot(data = auto_data, aes(x = price, y = engine_size)) +
+  geom_point(aes(color = fuel_type, shape = fuel_type))
 
 
 # hypothesis test: does high horsepower means higher price?
@@ -145,12 +138,265 @@ cor.test(auto_data$engine_size,auto_data$compression_ratio)
 
 
 
+# Task 2: Sales Data Munging
+library(readr)
+library(reshape2)
+library(tidyverse)
+library(RColorBrewer)
+
+
+# Load the data
+df <- read_csv("Sales.csv")
+
+# Print the shape of the data
+(dim(df))
+(head(df))
+
+
+# Check column info of the data
+(str(df))
+spec(df)
+
+# Check for missing values
+colSums(is.na(df))
+# print(heatmap(is.na(df), col = "hot"))
+
+# now missing values for invoice
+impute_invoice <- function() {
+  x <- sample(c(100:999), 10, replace = T)
+  y <- sample(c(10:99), 10, replace = T)
+  z <- sample(c(100:999), 10, replace = T)
+  a <- paste0(x, "-", y, "-", z)
+  return (a)
+}
+
+
+df[is.na(df$'Invoice ID'),1] <- impute_invoice()
+df
+
+colSums(is.na(df))
+
+# now missing values for unit price
+df[is.na(df$'Unit price'),c('Unit price','Total','Tax 5%','Quantity')]
+df$'Unit price' <- ifelse(is.na(df$'Unit price'), (df$Total - df$'Tax 5%') / df$Quantity, df$'Unit price')
+
+
+# missing values for Quantity
+head(df[is.na(df$'Quantity'),])
+df$'Quantity' <- ifelse(is.na(df$'Quantity'), (df$Total - df$'Tax 5%') %/% df$'Unit price', df$'Quantity')
+colSums(is.na(df))
+
+# missing values for Total
+df[is.na(df$'Total'),c('Unit price','Total','Tax 5%','Quantity')]
+df$'Total' <- ifelse(is.na(df$'Total'), (df$'Unit price' * df$'Quantity') + df$'Tax 5%', df$'Total')
+colSums(is.na(df))
+
+# missing values for Tax 5%
+df[is.na(df$'Tax 5%'),c('Unit price','Total','Tax 5%','Quantity')]
+df$'Tax 5%' <- ifelse(is.na(df$'Tax 5%'), (df$'Total' - (df$'Unit price') * df$'Quantity'), df$'Total')
+colSums(is.na(df))
+
+# missing values for Time
+df[is.na(df$Time),c('Date', 'Time')]
+random_time <- as.character(sample(df$Time,1))
+df[is.na(df$Time),'Time'] <- random_time
+df$Time <- gsub(":00", "",df$Time)
+
+# missing values for Payment
+df[is.na(df$Payment),]
+unique(df$Payment)
+
+most_payment <-  names(which.max(table(df$Payment)))
+df[is.na(df$Payment),'Payment'] <- most_payment
+
+# missing values for Payment
+
+df[is.na(df$'gross margin percentage'),
+   c('Total', 'gross income', 'gross margin percentage')]
+df[,c('Total', 'gross income', 'gross margin percentage')]
+
+df[is.na(df$'gross margin percentage'),'gross margin percentage'] <- unique(df$'gross margin percentage')[1]
+
+colSums(is.na(df))
+
+# # Convert the date column to datetime format
+df$Date <- as.Date(mdy(df$Date))
+
+# Plot the distribution of different branches
+
+
+branch_counts <- table(df$Branch)
+
+# Choose a color palette
+colors <- brewer.pal(length(branch_counts), "Paired")
+
+
+explode <- rep(0, length(branch_counts))
+explode[which.max(branch_counts)] <- 0.1
+
+ggplot(data.frame(Branch = names(branch_counts), Count = as.numeric(branch_counts)), aes(x = "", y = Count, fill = Branch)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar("y", start = 0) +
+  geom_text(aes(label = paste0(round(Count / sum(branch_counts) * 100, 1), "%")), 
+            position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = colors) +
+  theme_void() +
+  theme(legend.position = "right",
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        plot.subtitle = element_text(hjust = 0.5, size = 12)) +
+  labs(title = "Branch Distribution")
+
+# Plot the distribution of different cities
+ggplot(df, aes(x = City, fill = City)) +
+  geom_bar(width = 0.6, color = "white") +
+  scale_fill_manual(values = brewer.pal(length(unique(df$City)), "Paired")) +
+  labs(title = "Distribution of Different Cities",
+       x = "City",
+       y = "Count") +
+  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5, size = 3.5) +
+  ylim(0, 400) +
+  theme_minimal() +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        axis.title = element_text(size = 12),
+        legend.position = "right",
+        legend.title = element_blank())
+
+# Plot the distribution of customer types
+ggplot(df, aes(x = df$`Customer type`)) + 
+  geom_bar(width = 0.5, color = "white", fill='#B33F62') +
+  labs(title = "Distribution of customer types", x = "Customer type", y = "Count") + 
+  geom_text(stat = "count", aes(label = comma(..count..)), vjust = -0.5, size = 3.5) +
+  ylim(0,600) + 
+  theme_minimal() +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        axis.title = element_text(size = 12),
+        legend.position = "right",
+        legend.title = element_blank())
+
+
+
+# Plot the distribution of genders
+ggplot(data.frame(Gender = names(table(df$Gender)), Count = as.numeric(table(df$Gender))), aes(x = "", y = Count, fill = Gender)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar("y", start = 0) +
+  geom_text(aes(label = paste0(round(Count / sum(branch_counts) * 100, 1), "%")), 
+            position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = colors) +
+  theme_void() +
+  theme(legend.position = "right",
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        plot.subtitle = element_text(hjust = 0.5, size = 12)) +
+  labs(title = "Gender Distribution")
+
+
+# Product Line
+
+table(df$`Product line`)
+
+ggplot(df, aes(x = df$`Product line`, fill = df$`Product line`)) +
+  geom_bar(width = 0.6, color = "white") +
+  scale_fill_manual(values = brewer.pal(length(unique(df$`Product line`)), "Paired")) +
+  labs(title = "Distribution of Different Cities",
+       x = "City",
+       y = "Count") +
+  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5, size = 3.5) +
+  ylim(0, 200) +
+  theme_minimal() +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        axis.title = element_text(size = 12),
+        legend.position = "right",
+        legend.title = element_blank())
+
+
+# Plot the distribution of unit prices
+plot(density(df$`Unit price`,na.rm = T), main = 'Distribution of Unit Price')
+
+# plot scatterplot between total and rating with respect to Branch
+ggplot(data=df, aes(x= Total, y= Rating))+
+  geom_point(aes(color=Branch, shape=Branch))
+
+
+
+# Hypothesis testing: Does gross income increase when total sales increase?
+cor.test(df$Total, df$`gross income`)
+# it is indeed true, and is statistically significant
+
+# Plot the relationship between total sales and gross income
+ggplot(df, aes(x = Total, y = `gross income`)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(
+    title = "Relationship between Total and Gross Income",
+    x = "Total",
+    y = "Gross Income"
+  ) +
+  theme_classic() +
+  xlim(0,1200) + ylim(0,60)
+
+# Plot the relationship between customer type and total sales
+ggplot(df, aes(x = Total, color = `Customer type`)) + 
+  geom_density() + 
+  labs(title = "Relationship between customer type and total sales", x = "Total sales", y = "Density")
+
+
+# Which branch sales has the highest ratings?
+df %>% 
+  group_by(Branch) %>% 
+  summarise(mean_rating = mean(Rating)) %>% 
+  arrange(desc(mean_rating))
+
+# which product is used by different group of Gender
+
+product_usage <- df %>%
+  group_by(Gender, `Product line`) %>%
+  summarize(Count = n()) %>%
+  arrange(desc(Count))
+
+
+ggplot(product_usage, aes(x = `Product line`, y = Count, fill = Gender)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    title = "Product Usage by Gender",
+    x = "Product Line",
+    y = "Count",
+    fill = "Gender"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.3),
+    legend.position = "right"
+  )
+
+
+product_usage_city <- df %>%
+  group_by(City, `Product line`) %>%
+  summarize(Count = n()) %>%
+  arrange(desc(Count))
+
+# Create the plot
+ggplot(product_usage_city, aes(x = `Product line`, y = Count, fill = City)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    title = "Product Usage by City",
+    x = "Product Line",
+    y = "Count",
+    fill = "City"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.3),
+    legend.position = "right"
+  ) + ylim(0,80)
 
 
 
 
-
-
+# Task 3: Text Mining and Word Cloud (Used both Python and R)
 
 library(tm)
 library(Rgraphviz)
