0266d2034b32e809d81ad54f2a23af6ba14849a7
Suman Paudel <dastonsuman1997@gmail.com>
2024-06-24 20:57:50 +0545

    unsupervised learning

---
+++ unsupervised models.R
@@ -0,0 +1,21 @@
+# PCA looks to find a low- dimensional representation of the 
+# observations that explain a good 
+# fraction of the variance;
+
+# Clustering looks to find 
+# homogeneous subgroups among 
+# the observations or cases.
+
+
+# In K-means clustering, we seek 
+# to partition the observations 
+# into a pre-specified number of 
+# clusters.
+
+
+# On the other hand, in 
+# hierarchical clustering, we do 
+# not know in advance how many 
+# clusters we want and we use 
+# “dendogram” to find the 
+# number of clusters for the data
